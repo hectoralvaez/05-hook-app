23b445563e464d948a6f72eb48ce1bb7a39f1d9e
Héctor <hectoralvaez@gmail.com>
2022-10-15 09:51:51 +0200

    🪝 115. useState (Funciona! Creamos "state" para poder desestructurar el contenido y luego recuperarlo con el spread operator "...")

--- src/01-useState/CounterApp.jsx
+++ src/01-useState/CounterApp.jsx
@@ -2,12 +2,12 @@ import { useState } from "react";
 
 export const CounterApp = () => {
 
-    const [{counter1, counter2, counter3, }, setCounter] = useState({
+    const [state, setCounter] = useState({
         counter1: 10,
         counter2: 20,
         counter3: 30,
     })
-
+    const {counter1, counter2, counter3 } = state
     return (
         <>
             <h1>Counter1: { counter1 }</h1>
@@ -19,9 +19,8 @@ export const CounterApp = () => {
                 className="btn"
                 onClick={ 
                     () => setCounter( {
+                        ...state,
                         counter1: counter1 + 1,
-                        counter2,
-                        counter3,
                     } )
                 }>+1</button>
         </>
