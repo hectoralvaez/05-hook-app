3a124dbdeb68193bf46c85a945fb08b0e89738f3
Héctor <hectoralvaez@gmail.com>
2022-10-15 08:55:35 +0200

    🪝 115. useState (Startup: funciona con un Objeto en "useState")

--- src/01-useState/CounterApp.jsx
+++ src/01-useState/CounterApp.jsx
@@ -2,14 +2,22 @@ import { useState } from "react";
 
 export const CounterApp = () => {
 
-    const [counter, setCounter] = useState(10)
+    const [counter, setCounter] = useState({
+        counter1: 10,
+        counter2: 20,
+        counter3: 30,
+    })
 
     return (
         <>
-            <h1>Counter: { counter }</h1>
+            <h1>Counter: { counter.counter1 }</h1>
             <hr />
 
-            <button className="btn" onClick={ () => setCounter( counter + 1 ) }>+1</button>
+            <button
+                className="btn"
+                onClick={ 
+                    () => setCounter( counter + 1 )
+                }>+1</button>
         </>
     );
 };
