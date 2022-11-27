cd88d6d37d08e3b66bbc125ab005c2116d156fcc
Héctor <hectoralvaez@gmail.com>
2022-11-27 13:56:36 +0100

    🪝 127. Conectando ambos Hooks (EJERCICIO- Mi solución)

--- src/03-examples/MultipleCustomHooks.jsx
+++ src/03-examples/MultipleCustomHooks.jsx
@@ -1,20 +1,23 @@
 import { useFetch } from "../hooks/useFetch";
+import { useCounter } from "../hooks/useCounter";
 
 export const MultipleCustomHooks = () => {
-    
-    const { data, isLoading, hasError } = useFetch('https://www.breakingbadapi.com/api/quotes/1');
+    const { counter, increment } = useCounter(1);
+    const { data, isLoading, hasError } = useFetch(`https://www.breakingbadapi.com/api/quotes/${counter}`);
 
     // const { author, quote } = data; //Da error
     const { author, quote } = !!data && data[0];  //Hacemos condicional para evitar el "null" de la 'data'
     
     console.log({data, isLoading, hasError})
     console.log(data)
+    console.log(counter);
     
     return (
         <>
             <h1>Breaking Bad Quotes</h1>
             <hr />
 
+            <button className="btn btn-primary" onClick={ () => increment(1) }>Next quote</button>
             {
                 isLoading
                     ? (
