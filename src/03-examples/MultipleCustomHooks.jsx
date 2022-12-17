b9a60d1b0f2e16690099a2ba78c480b4a2f23d08
Héctor <hectoralvaez@gmail.com>
2022-12-17 08:34:54 +0100

    🔴 ERROR 500. Cambio url breakingbadapi.com > breakingbadquotes.xyz.

--- src/03-examples/MultipleCustomHooks.jsx
+++ src/03-examples/MultipleCustomHooks.jsx
@@ -3,7 +3,9 @@ import { useCounter } from "../hooks/useCounter";
 
 export const MultipleCustomHooks = () => {
     const { counter, increment } = useCounter(1);
-    const { data, isLoading, hasError } = useFetch(`https://www.breakingbadapi.com/api/quotes/${counter}`);
+    // Actualmente este dominio está dando "error 500"
+    // const { data, isLoading, hasError } = useFetch(`https://www.breakingbadapi.com/api/quotes/${counter}`);
+    const { data, isLoading, hasError } = useFetch(`https://api.breakingbadquotes.xyz/v1/quotes/${counter}`);
 
     // const { author, quote } = data; //Da error
     const { author, quote } = !!data && data[0];  //Hacemos condicional para evitar el "null" de la 'data'
