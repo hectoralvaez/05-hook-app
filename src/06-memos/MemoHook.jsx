3decf2973c5502039c0185b726ebbda8239f76e4
Héctor <hectoralvaez@gmail.com>
2022-12-28 23:53:03 +0100

    🪝 132. useMemo - Funcionando sin optimizar (sin useMemo) (v2)

--- src/06-memos/MemoHook.jsx
+++ src/06-memos/MemoHook.jsx
@@ -1,10 +1,19 @@
 import { useState } from "react";
 import { useCounter } from "../hooks";
 
+
+const heavyStuff = ( iterationNumber = 100 ) => {
+    for ( let i = 0; i < iterationNumber; i++ ) {
+        console.log(`Ahi vamos...`);
+    }
+
+    return `${ iterationNumber } iteraciones realizadas`;
+}
+
 export const MemoHook = () => {
 
-    const { counter, increment } = useCounter( 10 );
-    const [show, setShow] = useState(false)
+    const { counter, increment } = useCounter( 4000 );
+    const [show, setShow] = useState(false);
 
     return (
         <>
@@ -13,6 +22,7 @@ export const MemoHook = () => {
             </h1>
             <hr />
 
+            <h4>{ heavyStuff( counter ) }</h4>
             <button 
                 className="btn btn-primary"
                 onClick={ () => increment() }    
