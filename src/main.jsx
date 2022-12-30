57e9cb3977e60638dea044827362521a6502efc7
Héctor <hectoralvaez@gmail.com>
2022-12-30 22:57:44 +0100

    🪝 135. Tarea Memorize. Startup.

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import { CallbackHook } from './06-memos/CallbackHook'
+import { Padre } from './07-tarea-memo/Padre'
+// import { CallbackHook } from './06-memos/CallbackHook'
 // import { MemoHook } from './06-memos/MemoHook'
 // import { Memorize } from './06-memos/Memorize'
 // import { Layout } from './05-useLayoutEffect/Layout'
@@ -16,6 +17,6 @@ import './index.css'
 ReactDOM.createRoot(document.getElementById('root')).render(
   // <React.StrictMode>
     // <MultipleCustomHooks />
-    <CallbackHook />
+    <Padre />
   // </React.StrictMode>
 )
