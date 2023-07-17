b26035dbe2d2d3f6d2eb91af6433cec9ffb7273e
Héctor <hectoralvaez@gmail.com>
2023-07-17 23:51:19 +0200

    🪝 159. NavLink

--- src/09-useContext/Navbar.jsx
+++ src/09-useContext/Navbar.jsx
@@ -1,11 +1,35 @@
-import { Link } from "react-router-dom"
+import { Link, NavLink } from "react-router-dom";
 
 export const Navbar = () => {
-  return (
-      <>
-          <Link to="/">Home</Link>
-          <Link to="/about">About</Link>
-          <Link to="/login">Login</Link>
-      </>
-    )
-}
+    return (
+        <nav className="navbar navbar-expand-lg bg-body-tertiary navbar-dark bg-dark">
+            <div className="container-fluid">
+                <Link className="navbar-brand" to="/">
+                    useContext
+                </Link>
+                <div
+                    className="collapse navbar-collapse"
+                    id="navbarSupportedContent"
+                >
+                    <ul className="navbar-nav me-auto mb-2 mb-lg-0">
+                        <NavLink 
+							className={"nav-link"}
+							to="/">
+                            Home
+                        </NavLink>
+                        <NavLink
+							className={"nav-link"}
+							to="about">
+                            About
+                        </NavLink>
+                        <NavLink
+							className={"nav-link"}
+							to="login">
+                            Login
+                        </NavLink>
+                    </ul>
+                </div>
+            </div>
+        </nav>
+    );
+};
